4a7865987f714fd3a7b51a3fe6cbbb8a9354a7db
Max Myers <mmyers@stackleader.com>
2024-03-28 16:24:51 -0400

    Fix file output and regex

--- fileFindAndReplace.py
+++ fileFindAndReplace.py
@@ -1,17 +1,30 @@
+import os
 import re
-import sys
 
-file_path = sys.argv
+def write_matching_lines_to_file(directory_path, regex_pattern, output_file):
+    if not os.path.isdir(directory_path):
+        print("Invalid directory path.")
+        return
 
-def print_matching_lines(file_path, regex_pattern):
-    with open(file_path, 'r') as file:
-        line_number = 0
-        for line in file:
-            line_number += 1
-            match = re.search(regex_pattern, line)
-            if match:
-                print(f"Line {line_number}: {line.strip()}")
+    with open(output_file, 'w') as outfile:
+        for root, dirs, files in os.walk(directory_path):
+            for filename in files:
+                if filename.endswith(".jsp"):
+                    fileContent = ""
+                    file_path = os.path.join(root, filename)
+                    matches_found = False
+                    with open(file_path, 'r') as file:
+                        line_number = 0
+                        for line in file:
+                            line_number += 1
+                            match = re.search(regex_pattern, line)
+                            if match:
+                                matches_found = True
+                                fileContent += f"Line {line_number}: {line.strip()}\n"
+                    if matches_found:
+                        outfile.write(f'\n###################### File: {filename} ###################### \n' + fileContent)
 
-file_path = '../TxEIS-Project/registration/WebRoot/report/viewReportDialog.jsp'  # Replace with your file path
-regex_pattern = r'\.(js|css)(?![p])\b'  # Replace with your regex pattern
-print_matching_lines(file_path, regex_pattern)
+directory_path = '../TxEIS-Project/registration'
+regex_pattern = r'\.(js|css)(?![p(])\b'
+output_file = 'output.txt'
+write_matching_lines_to_file(directory_path, regex_pattern, output_file)
